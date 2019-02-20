fbcc220dd14d045e68f6b15cedc3b38c865228ed
anjelicakay <aktowner@gmail.com>
2019-02-19 21:44:42 -0600

    destroy and update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,12 +16,37 @@
       </div>
       <button v-on:click="createProduct">Create</button>
     </div>
-    <h1>ALL Products</h1>
+
+    <h1>All Products</h1>
     <div v-for="product in products">
       <h2>{{ product.name }}</h2>
       <img v-bind:src="product.image_url" v-bind:alt="product.name">
-      <p>Description: {{ product.description }}</p>
-      <p>Price: {{ product.formatted.price }}</p>
+      <div>
+        <button v-on:click="showProduct(product)">More Info</button>
+      </div>
+      <div v-if="product === currentProduct">
+        <p>Description: {{ product.description }}</p>
+        <p>Price: {{ product.formatted.price }}</p>
+        <div>
+          <h4>Edit Recipe</h4>
+          <div>
+            <div>
+              Name: <input v-model="product.name">
+            </div>
+            <div>
+              Decription: <input v-model="product.description">
+            </div>
+            <div>
+              Price: <input v-model="product.price">
+            </div>
+            <div>
+              Image: <input v-model="product.image_url">
+            </div>
+            <button v-on:click="updateProduct(product)">Update</button>
+            <button v-on:click="destroyProduct(product)">Delete</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,7 +70,8 @@ export default {
       newProductName: "",
       newProductDescription: "",
       newProductPrice: "",
-      newProductImageUrl: ""
+      newProductImageUrl: "",
+      currentProduct: {}
     };
   },
   created: function() {
@@ -55,7 +81,14 @@ export default {
     });
   },
   methods: {
-    createProduct: function () {
+    showProduct: function(inputProduct) {
+      if (this.currentProduct === inputProduct){
+        this.currentProduct = {};
+      } else {
+        this.currentProduct = inputProduct;
+      }
+    },
+    createProduct: function (){
       console.log("Create the Product..");
       var params={
                   name: this.newProductName,
@@ -68,6 +101,27 @@ export default {
         .then(response => {
           console.log("Success", response.data);
           this.products.push(response.data);
+        });
+    },   
+    updateProduct: function(inputProduct) {
+      var params = {
+                    name: inputProduct.name,
+                    description: inputProduct.description,
+                    price: inputProduct.price,
+                    image_url: inputProduct.image_url
+                  };
+      axios.patch("/api/products/" + inputProduct.id, params)
+        .then(response => {
+          console.log("success", response.data);
+          inputProduct = response.data;
+        });            
+    },
+    destroyProduct: function(inputProduct) {
+      axios.delete("/api/products/" + inputProduct.id)
+        .then(response => {
+          console.log("Success", response.data);
+          var index = this.products.indexOf(inputProduct);
+          this.products.splice(index,1);
         });
     }
   }
